ca724af5a3aab125719d5cbc83ca772d67a8457b
Marnix Kok <marnix@xinsolutions.co.nz>
2017-09-10 20:09:37 +1200

    Changes:
    * removed beans
    * removed page service
    * removed dependencies on the repository data

--- site/src/main/java/nz/xinsolutions/beans/Blogdocument.java
+++
@@ -1,9 +0,0 @@
-package nz.xinsolutions.beans;
-
-import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
-import org.hippoecm.hst.content.beans.Node;
-
-@HippoEssentialsGenerated(internalName = "xinmods:blogdocument")
-@Node(jcrType = "xinmods:blogdocument")
-public class Blogdocument extends Page {
-}
