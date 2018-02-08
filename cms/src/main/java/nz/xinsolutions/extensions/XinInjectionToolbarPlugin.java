a5d3acdcfac871f07b13139792956d00cf076466
Marnix Kok <marnix@xinsolutions.co.nz>
2018-02-08 20:22:49 +1300

    Changes:
    * Created a toolbar workflow plugin that is able to launch external application for a document path.

--- cms/src/main/java/nz/xinsolutions/extensions/XinInjectionToolbarPlugin.java
+++ cms/src/main/java/nz/xinsolutions/extensions/XinInjectionToolbarPlugin.java
@@ -1,17 +1,10 @@
 package nz.xinsolutions.extensions;
 
 import org.apache.commons.lang.ArrayUtils;
-import org.apache.wicket.Component;
-import org.apache.wicket.behavior.AttributeAppender;
-import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
-import org.hippoecm.addon.workflow.StdWorkflow;
 import org.hippoecm.addon.workflow.WorkflowDescriptorModel;
 import org.hippoecm.frontend.plugin.IPluginContext;
 import org.hippoecm.frontend.plugin.config.IPluginConfig;
 import org.hippoecm.frontend.plugins.reviewedactions.AbstractDocumentWorkflowPlugin;
-import org.hippoecm.frontend.plugins.standards.icon.HippoIcon;
-import org.hippoecm.frontend.skin.Icon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -45,86 +38,46 @@ public class XinInjectionToolbarPlugin extends AbstractDocumentWorkflowPlugin {
         String title = getTitle(config);
         String iconId = getIconId(config);
         String subMenu = getSubMenu(config);
-        String action = (String) config.get("action");
+        String action = getAction(config);
         String[] validTypes = getValidTypes(config);
         
         try {
             Node handle = ((WorkflowDescriptorModel) getDefaultModel()).getNode();
             String docType = ((Node)handle.getNodes().next()).getPrimaryNodeType().getName();
     
-            LOG.info("toolbar for doc: " + handle.getPath());
+            LOG.info("Toolbar for doc: " + handle.getPath());
     
-            StdWorkflow wf = null;
-            
             // add a button
-            this.add(wf = new StdWorkflow(config.getName(), config.getName()) {
-    
-                /**
-                 * {@inheritDoc}
-                 */
-                public String getSubMenu() {
-                    return subMenu;
-                }
-    
-                @Override
-                protected void onRender() {
-                    super.onRender();
-                }
-    
-                /**
-                 * {@inheritDoc}
-                 */
-                @Override
-                protected Component getIcon(String id) {
-                    return HippoIcon.fromSprite(id, Icon.valueOf(iconId));
-                }
-    
-                /**
-                 * {@inheritDoc}
-                 */
-                @Override
-                protected IModel<String> getTitle() {
-                    return new Model<String>(title);
-                }
-    
-                /**
-                 * {@inheritDoc}
-                 */
-                @Override
-                public boolean isVisible() {
-                    
-                    // no restrictions?
-                    if (validTypes == null) {
-                        return true;
-                    }
-                    
-                    return ArrayUtils.contains(validTypes, docType);
-                }
-    
-            });
-            
-            wf.add(
-                new AttributeAppender("onclick", new Model(String.format("alert('interestink: %s'", action)) )
-            );
-    
-            wf.add(new AttributeAppender("data-url", new Model(action) ));
-    
             this.add(
-                new AttributeAppender(
-                    "onclick",
-                    new Model(String.format("alert('interestink: %s'", action))
-                )
+                new JavascriptEnabledStdWorkflow(config.getName(), subMenu, iconId, title,  action, handle.getPath()) {
+
+                    /**
+                     * {@inheritDoc}
+                     */
+                    @Override
+                    public boolean isVisible() {
+
+                        // no restrictions?
+                        if (validTypes == null) {
+                            return true;
+                        }
+
+                        return ArrayUtils.contains(validTypes, docType);
+                    }
+
+                }
             );
-    
-            this.add(new AttributeAppender("data-url", new Model(action) ));
-    
-    
         }
         catch (RepositoryException rEx) {
             LOG.error("Could not retrieve handler, caused by: ", rEx);
         }
     }
-   
+
+    private String getAction(IPluginConfig config) {
+        return (String) config.get("action");
+    }
+
+
     /**
      * @return a list of valid types
      */
@@ -158,5 +111,7 @@ public class XinInjectionToolbarPlugin extends AbstractDocumentWorkflowPlugin {
                     ? (String) config.get("title")
                     : "Unnamed";
     }
+
+
     
 }
