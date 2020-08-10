a3924e70983409b9fc1d42eea7cf215ca6f34313
Marnix Kok <marnix@xinsolutions.co.nz>
2020-08-10 22:28:01 +1200

    Changes:
    * updated to 14.2.2

--- start-local.sh
+++ start-local.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+mkdir -p storage/
 # export AWS_HIPPOBUS_ARN=yourarn
-mvn clean verify && mvn -P cargo.run -Drepo.path=storage-dev 
+mvn clean verify && mvn -P cargo.run -Drepo.path=storage/dev 
