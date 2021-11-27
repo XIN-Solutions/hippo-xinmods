f8d8ec9d1faa801abb21c21dd2a777f15629a089
Marnix Kok <marnix@xinsolutions.co.nz>
2021-11-28 00:03:49 +1300

    Changes:
    * Update to Bloomreach 14.6.3
    * Better auth0 plugin
    * fix imagefocus plugin
    * add simple xpath subset matcher for multi-fetch endpoints.

---
+++ site/webapp/src/main/java/nz/xinsolutions/services/XpathSelectorMatcher.java
@@ -0,0 +1,191 @@
+package nz.xinsolutions.services;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * Author: Marnix Kok
+ *
+ * Purpose:
+ *
+ *   A class that helps identify whether a recursion breadcrumb through a datastructure
+ *   matches a particular xpath subset syntax.
+ *
+ *      - /from/the/root/exact
+ *      - relative/to/anywhere
+ *      - /from/the/root//a-random-child
+ *
+ *   Constraints:
+ *
+ *      - you cannot have more than one use of '//'
+ *      - we don't match on any attributes, only node paths
+ *
+ */
+public class XpathSelectorMatcher {
+
+    /**
+     * Logger
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(XpathSelectorMatcher.class);
+    public static final int IDX_HEAD_PART = 0;
+    public static final int IDX_TAIL_PART = 1;
+
+    /**
+     * This is the function that determines whether a particular breadcrumb of node names
+     * matches the xpathSelector based on the constraints outlined in the class description.
+     *
+     * @param pathBreadcrumb    a set of elements with path breadcrumbs
+     * @param xpathSelector     the xpath selector string to match against.
+     * @return
+     */
+    public boolean matchesXpathSelector(List<String> pathBreadcrumb, String xpathSelector) {
+
+        if (containsBasePath(xpathSelector)) {
+            LOG.debug("Found wildcard selector");
+            String startMatch = getMatchHead(xpathSelector);
+            String relativeEnd = getMatchTail(xpathSelector);
+
+            return (
+                // if the string starts with // we can just assume the start matches.
+                (StringUtils.isBlank(startMatch) || matchesFromStart(pathBreadcrumb, startMatch)) &&
+                matchesFromEnd(pathBreadcrumb, relativeEnd)
+            );
+        }
+        else if (isRelativeSelector(xpathSelector)) {
+            LOG.debug("Found relative selector");
+            return matchesFromEnd(pathBreadcrumb, xpathSelector);
+        }
+        else if (isAbsoluteSelector(xpathSelector)) {
+            LOG.debug("Found absolute selector");
+            return completeMatch(pathBreadcrumb, xpathSelector);
+        }
+
+        return false;
+    }
+
+    /**
+     * @return true if the entire xpath selector matches the path breadcrumb
+     */
+    protected boolean completeMatch(List<String> pathBreadcrumb, String xpathSelector) {
+
+        if (StringUtils.isEmpty(xpathSelector)) {
+            return false;
+        }
+
+        if (pathBreadcrumb.size() != xpathSelector.split("/").length - 1) {
+            return false;
+        }
+
+        return this.matchesFromStart(pathBreadcrumb, xpathSelector);
+    }
+
+
+    /**
+     * @return true if the path breadcrumb matches from the start
+     */
+    protected boolean matchesFromStart(List<String> pathBreadcrumb, String startMatch) {
+        // expecting the string to start with a '/', if it does, let's strip it.
+        String splitThis = startMatch;
+        if (startMatch.charAt(0) == '/') {
+            splitThis = startMatch.substring(1);
+        }
+
+        String[] pathParts = splitThis.split("/");
+
+        // can't possibly match the entire thing if it isn't at least the same size.
+        if (pathParts.length > pathBreadcrumb.size()) {
+            return false;
+        }
+
+        // iterate through all path parts and make sure they equal that in the breadcrumb.
+        boolean allMatched = true;
+        for (int elIdx = 0; elIdx < pathParts.length; ++elIdx) {
+            String pathElement = pathParts[elIdx];
+            allMatched &= pathBreadcrumb.get(elIdx).equals(pathElement);
+        }
+
+        // should be true if all elements were equal, otherwise AND will have made it false.
+        return allMatched;
+    }
+
+    /**
+     * @return true if the end of the breadcrumb matches the xpath selector
+     */
+    protected boolean matchesFromEnd(List<String> pathBreadcrumb, String xpathSelector) {
+        String[] pathParts = xpathSelector.split("/");
+
+        // can't possibly match the entire thing if it isn't at least the same size.
+        if (pathParts.length > pathBreadcrumb.size()) {
+            return false;
+        }
+
+        // iterate through all path parts and make sure they equal that in the breadcrumb.
+        boolean allMatched = true;
+        for (int elIdx = pathParts.length - 1, revIdx = 1; elIdx >= 0; --elIdx, ++revIdx) {
+            String pathElement = pathParts[elIdx];
+            int breadcrumbElIdx = pathBreadcrumb.size() - revIdx;
+            allMatched &= pathBreadcrumb.get(breadcrumbElIdx).equals(pathElement);
+        }
+
+        // should be true if all elements were equal, otherwise AND will have made it false.
+        return allMatched;
+    }
+
+    /**
+     * @return the part of the selector that matches pre '//' (the thing that should match the absolute path at the start)
+     */
+    protected String getMatchHead(String xpathSelector) {
+        if (StringUtils.isEmpty(xpathSelector)) {
+            throw new IllegalArgumentException("Should not invoke method with empty value.");
+        }
+        return xpathSelector.split("//")[IDX_HEAD_PART];
+    }
+
+    /**
+     * @return the part of the selector that matches past '//' (the thing that should match the end of the breadcrumb)
+     */
+    protected String getMatchTail(String xpathSelector) {
+        if (StringUtils.isEmpty(xpathSelector)) {
+            throw new IllegalArgumentException("Should not invoke method with empty value.");
+        }
+        String[] pathParts = xpathSelector.split("//");
+        if (pathParts.length > 2) {
+            throw new IllegalArgumentException("Do not support multiple wildcard match points.");
+        }
+        return pathParts[IDX_TAIL_PART];
+    }
+
+    /**
+     * @return true if the xpathSelector is expecting an absolute path match
+     */
+    protected boolean isAbsoluteSelector(String xpathSelector) {
+        if (StringUtils.isEmpty(xpathSelector)) {
+            return false;
+        }
+        return xpathSelector.charAt(0) == '/';
+    }
+
+    /**
+     * @return true if the xpathSelector is not expecting an absolute path match
+     */
+    protected boolean isRelativeSelector(String xpathSelector) {
+        if (StringUtils.isEmpty(xpathSelector)) {
+            return false;
+        }
+        return !this.isAbsoluteSelector(xpathSelector);
+    }
+
+    /**
+     * @return true if the xpath selector has a wildcard path match '//' in the string
+     */
+    protected boolean containsBasePath(String xpathSelector) {
+        if (StringUtils.isEmpty(xpathSelector)) {
+            return false;
+        }
+        return xpathSelector.contains("//");
+    }
+
+}
---
+++ site/webapp/src/test/java/nz/xinsolutions/services/XpathSelectorMatcherTest.java
@@ -0,0 +1,122 @@
+package nz.xinsolutions.services;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Purpose:
+ *
+ *      To do some testing on the xpath selector matcher class.
+ *
+ */
+public class XpathSelectorMatcherTest {
+
+    /**
+     * Matcher instance to interrogate
+     */
+    private XpathSelectorMatcher matcher = new XpathSelectorMatcher();
+
+    @Test
+    public void testMatchesXpathSelector() {
+        List<String> breadcrumb = Arrays.asList("item", "model", "component", "image", "thumbnail", "link");
+
+        // -- absolute
+
+        // 1. should match entire path
+        assertTrue(matcher.matchesXpathSelector(breadcrumb, "/item/model/component/image/thumbnail/link"));
+
+        // 2. should not match on partial path
+        assertFalse(matcher.matchesXpathSelector(breadcrumb, "/item/model/component/image/thumbnail"));
+
+        // 3. shouldn't match a path that doesn't exist
+        assertFalse(matcher.matchesXpathSelector(breadcrumb, "/ite/model/component/image/thumbnail"));
+
+        // -- relative
+        // 1. should match relative for entire path
+        assertTrue(matcher.matchesXpathSelector(breadcrumb, "item/model/component/image/thumbnail/link"));
+        // 2. should match relative for just a part of it
+        assertTrue(matcher.matchesXpathSelector(breadcrumb, "image/thumbnail/link"));
+        // 3. shouldn't work if not all elements are there
+        assertFalse(matcher.matchesXpathSelector(breadcrumb, "image/thumbnail"));
+
+        // -- wildcard match
+        // 1. should match for a few partials
+        assertTrue(matcher.matchesXpathSelector(breadcrumb, "/item/model//thumbnail/link"));
+        assertTrue(matcher.matchesXpathSelector(breadcrumb, "//thumbnail/link"));
+        assertTrue(matcher.matchesXpathSelector(breadcrumb, "/item/model/component/image//thumbnail/link"));
+        // 2. shouldn't match partial node names
+        assertFalse(matcher.matchesXpathSelector(breadcrumb, "/item/mode//thumbnail/link"));
+        assertFalse(matcher.matchesXpathSelector(breadcrumb, "/untrue/item/model//thumbnail/link"));
+    }
+
+    @Test
+    public void testAbsoluteSelector() {
+        assertTrue(matcher.isAbsoluteSelector("/my/absolute/selector"));
+        assertFalse(matcher.isAbsoluteSelector("my/relative/selector"));
+        assertFalse(matcher.isAbsoluteSelector(""));
+        assertFalse(matcher.isAbsoluteSelector("relative-selector"));
+    }
+
+    @Test
+    public void testRelativeSelector() {
+        assertTrue(matcher.isRelativeSelector("my/relative/selector"));
+        assertTrue(matcher.isRelativeSelector("relative-selector"));
+        assertFalse(matcher.isRelativeSelector(""));
+        assertFalse(matcher.isRelativeSelector("/my/absolute/selector"));
+    }
+
+    @Test
+    public void testWildcardExpressionTester() {
+        assertTrue(matcher.containsBasePath("//thumbnail/link"));
+        assertFalse(matcher.containsBasePath("my/relative/selector"));
+        assertTrue(matcher.containsBasePath("/item/model/component/image//thumbnail/link"));
+    }
+
+    @Test
+    public void testWildcardExtraction() {
+        String path = "/item/model/component/image//thumbnail/link";
+        assertEquals("/item/model/component/image", matcher.getMatchHead(path));
+        assertEquals("thumbnail/link", matcher.getMatchTail(path));
+    }
+
+    @Test
+    public void testCompleteMatch() {
+        List<String> breadcrumb = Arrays.asList("item", "model", "component", "image", "thumbnail", "link");
+        String path = "/item/model/component/image/thumbnail/link";
+        assertTrue(matcher.completeMatch(breadcrumb, path));
+
+        path = "/item/model/component/image/thumbnail/link/too-long";
+        assertFalse(matcher.completeMatch(breadcrumb, path));
+
+        path = "item/model/component/image/thumbnail/link/";
+        assertFalse(matcher.completeMatch(breadcrumb, path));
+
+        path = "/item_incorrect/model/component/image/thumbnail/link";
+        assertFalse(matcher.completeMatch(breadcrumb, path));
+    }
+
+
+    @Test
+    public void testMatchesFromEnd() {
+        List<String> breadcrumb = Arrays.asList("item", "model", "component", "image", "thumbnail", "link");
+
+        String path = "image/thumbnail/link";
+        assertTrue(matcher.matchesFromEnd(breadcrumb, path));
+
+        path = "image/thumbnail/link-incorrect";
+        assertFalse(matcher.matchesFromEnd(breadcrumb, path));
+
+        path = "image/thumbnail-incorrect/link";
+        assertFalse(matcher.matchesFromEnd(breadcrumb, path));
+
+        path = "image/link";
+        assertFalse(matcher.matchesFromEnd(breadcrumb, path));
+
+    }
+
+}
