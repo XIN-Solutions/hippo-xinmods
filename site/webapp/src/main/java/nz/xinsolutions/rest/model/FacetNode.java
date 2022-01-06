447dcc106cf93cb2a6b9058574ef21b86523e748
Marnix Kok <marnixkok+github@gmail.com>
2022-01-06 23:12:58 +1300

    Changes:
    * add faceted navigation endpoint

---
+++ site/webapp/src/main/java/nz/xinsolutions/rest/model/FacetNode.java
@@ -0,0 +1,58 @@
+package nz.xinsolutions.rest.model;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Author: Marnix Kok
+ * <p>
+ * Purpose:
+ */
+public class FacetNode {
+
+    private String sourceFacet;
+    private String facetPath;
+    private String displayName;
+    private Map<String, Integer> childFacets;
+    private List<Map<String, Object>> results;
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public Map<String, Integer> getChildFacets() {
+        return childFacets;
+    }
+
+    public void setChildFacets(Map<String, Integer> childFacets) {
+        this.childFacets = childFacets;
+    }
+
+    public List<Map<String, Object>> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Map<String, Object>> results) {
+        this.results = results;
+    }
+
+    public String getSourceFacet() {
+        return sourceFacet;
+    }
+
+    public void setSourceFacet(String sourceFacet) {
+        this.sourceFacet = sourceFacet;
+    }
+
+    public String getFacetPath() {
+        return facetPath;
+    }
+
+    public void setFacetPath(String facetPath) {
+        this.facetPath = facetPath;
+    }
+}
